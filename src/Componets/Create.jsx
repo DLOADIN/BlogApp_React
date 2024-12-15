6705bf75d406663e78bb1b7bb8176ec89d03a9e9
DLOADIN <m.david@alustudent.com>
2024-12-15 18:54:58 +0200

    Blog App

--- src/Componets/Create.jsx
+++ src/Componets/Create.jsx
@@ -23,7 +23,6 @@ function Create() {
       setAuthor('');
       history.push('/');
     })
-
   }
 
   return (
--- src/Componets/Home.jsx
+++ src/Componets/Home.jsx
@@ -5,16 +5,13 @@ import Bloglist from './Bloglist'
 import useFetch from './customHook'
 
 function Home() {
-
-    const {data: blogs, isPending, error} = useFetch('http://localhost:5000/blogs');
+  const {data: blogs, isPending, error} = useFetch('http://localhost:5000/blogs');
   return (
-  
     <div>
         {error && <div className='text-red-500 absolute left-[40%] top-[40%]'>{error}</div>}
         {isPending && <div className='text-2xl font-bold absolute left-[50%] top-[50%]'>Loading...</div>}
-        {blogs && < Bloglist blogs={blogs} tittle = "All Blogs"  />}
-        
-        </div>
+        {blogs && < Bloglist blogs={blogs} tittle = "All Blogs"  />}    
+    </div>
   )
 }
 
